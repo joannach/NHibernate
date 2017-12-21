d3babebce04e8dc918a63115d788ff0989ea4670
Asia <joanna.chrominska9@gmail.com>
2017-12-21 15:52:20 +0100

    Pierwsza

---
+++ NHibernateFirst/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace NHibernateFirst
+{
+    class Employee
+    {
+        public int id;      //daje dostep do identity z bazy (primary key) trwałego obiektu
+        public string name;
+        public Employee manager;
+
+        public string SayHello()
+        {
+            return $"Cos tam cos tam, said {name} ";
+        }
+    }
+}
--- NHibernateFirst/Program.cs
+++ NHibernateFirst/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using NHibernate;
+using NHibernate.Cfg;
 
 namespace NHibernateFirst
 {
@@ -11,5 +10,34 @@ namespace NHibernateFirst
         static void Main(string[] args)
         {
         }
+
+        static ISession OpenSession()
+        {
+            if (sessionFactory == null)
+            {
+                Configuration c = new Configuration();
+                c.AddAssembly(Assembly.GetCallingAssembly());
+                sessionFactory = c.BuildSessionFactory();
+            }
+            return sessionFactory.OpenSession();
+        }
+
+        static void CreateEmployeeAndSaveToDatabase(Employee e)
+        {
+            var employee = e;
+
+            using (ISession session = OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Save(employee); // jesli zapisujemy encje bez id to w tym momencie (wywolanie metody Save)
+                                            // spowoduje wydenerowanie tego id (klucza glownego, unikalnego)
+                    transaction.Commit();
+                }
+                Console.WriteLine("Saved " + employee.name + " to the database");
+            }
+        }
+
+        private static ISessionFactory sessionFactory;
     }
 }
